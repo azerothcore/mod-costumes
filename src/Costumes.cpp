fb18ed4a503e538ec5b012fed0e1aacbb61f5c8f
Helias <stefanoborzi32@gmail.com>
2022-05-22 21:10:36 +0200

    Revert "refactor: fix type"
    This reverts commit 6ee05c430a9f5c015028803dadb95f179e349eb5.

--- src/Costumes.cpp
+++ src/Costumes.cpp
@@ -161,15 +161,15 @@ bool Costumes::CanUseItem(Player *player, ItemTemplate const *item, InventoryRes
         char formattedTime[9]{};
         if (hours)
         {
-            sprintf(formattedTime, "%lld:%lld:%lld", hours, minutes, seconds);
+            sprintf(formattedTime, "%02ld:%02ld:%02ld", hours, minutes, seconds);
         }
         else if (minutes)
         {
-            sprintf(formattedTime, "%lld:%lld", minutes, seconds);
+            sprintf(formattedTime, "%02ld:%02ld", minutes, seconds);
         }
         else
         {
-            sprintf(formattedTime, "%llds", seconds);
+            sprintf(formattedTime, "%02lds", seconds);
         }
         player->GetSession()->SendNotification("Cooldown: %s", formattedTime);
 
